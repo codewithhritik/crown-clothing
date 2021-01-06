27a0b16b4016c75796617dbbcc70243dd76def6b
Hritik <hritikbagane@gmail.com>
2021-01-06 11:11:50 +0530

    Added Local Storage using redux-persist library

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
 import { createStore, applyMiddleware } from 'redux';
 // Really nice for debugging our redux code
 import logger from 'redux-logger';
+// Allows our browser to cache/Store
+import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares))
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+export const persistor = persistStore(store);
+
+export default { store, persistor };
